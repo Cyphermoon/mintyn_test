17a34d59fc2673b4ea2a95df4863697ea8e5ccd9
Cypher Moon <temperaturecaleb@gmail.com>
2022-11-18 00:08:44 +0100

    Created a date visualisation section

---
+++ src/components/DateStats.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DateStateHeader from './DateStateHeader'
+
+const DateStats = () => {
+    return (
+        <div className='bg-white p-2 flex-grow mr-4'>
+            <DateStateHeader />
+        </div>
+    )
+}
+
+export default DateStats
--- src/components/StatsSection.jsx
+++ src/components/StatsSection.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import DateStats from './DateStats'
 import InfoDisplay from './InfoDisplay'
 import InfoSummary, { SummaryItem } from './InfoSummary'
 import PaymentTransactionSection from './PaymentTransactionSection'
 
 const StatsSection = () => {
   return (
-    <section className='flex-grow px-5 py-5'>
+    <section className='flex-grow px-5 py-5 space-y-10'>
         <div className='flex justify-between'>
             <InfoDisplay name="Daily Transaction Volume" value={"2,342"} />
             <InfoDisplay name="Daily Transaction Value" value={"₦4,000,000"} />
@@ -13,19 +14,24 @@ const StatsSection = () => {
             <InfoDisplay name="Total Transaction Value" value={"₦4,000,000"} />
         </div>
 
-        <div className='space-y-2'>
-            <InfoSummary headerText={"Orders"}>
-                <SummaryItem text="Pending Orders" value="20" color="text-accent"/>
-                <SummaryItem text="Reconciled Orders" value="80" color="text-secondary"/>
-                <SummaryItem text="Total Payment" value="80" color="text-primary"/>
-            </InfoSummary>
+        <section className='flex'>
+         <DateStats />
 
-            <InfoSummary headerText={"Payments"}>
-                <SummaryItem text="Un-reconciled Payments" value="20" color="text-accent"/>
-                <SummaryItem text="Reconciled Orders" value="80" color="text-secondary"/>
-                <SummaryItem text="Total Payment" value="80" color="text-primary"/>
-            </InfoSummary>
-        </div>
+            <div className='space-y-2 w-3/12'>
+                <InfoSummary headerText={"Orders"}>
+                    <SummaryItem text="Pending Orders" value="20" color="text-accent"/>
+                    <SummaryItem text="Reconciled Orders" value="80" color="text-secondary"/>
+                    <SummaryItem text="Total Payment" value="80" color="text-primary"/>
+                </InfoSummary>
+
+                <InfoSummary headerText={"Payments"}>
+                    <SummaryItem text="Un-reconciled Payments" value="20" color="text-accent"/>
+                    <SummaryItem text="Reconciled Orders" value="80" color="text-secondary"/>
+                    <SummaryItem text="Total Payment" value="80" color="text-primary"/>
+                </InfoSummary>
+            </div>
+
+        </section>
 
         <PaymentTransactionSection />
         
