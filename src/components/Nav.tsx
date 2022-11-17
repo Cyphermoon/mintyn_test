7af0eb8f3f4d25d16e1f38a200d861823af49f57
Cypher Moon <temperaturecaleb@gmail.com>
2022-11-17 15:00:56 +0100

    styled the list items

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar'
 
 
 const ListItem = ({ children, className }: listItemType) => {
-    return <li className={`${className ? className : ""}`}>
+    return <li className={`${className ? className : ""} text-title_md text-gray-700`}>
         {children}
     </li>
 }
@@ -14,7 +14,7 @@ const Nav = () => {
         <nav className='flex justify-between items-center bg-white drop-shadow-2xl py-3 px-5'>
             <h1 className='text-primary'>TransMonitor</h1>
             <SearchBar placeholder='Search...' />
-            <ul className='flex justify-between w-11/12 max-w-sm'>
+            <ul className='flex justify-between items-center w-11/12 max-w-sm'>
                 <ListItem>
                     <a href='/'>Support</a>
                 </ListItem>
@@ -25,11 +25,11 @@ const Nav = () => {
                     <small>3</small>
                 </ListItem>
                 <ListItem className='flex'>
-                    <div className='flex flex-col'>
+                    <div className='flex flex-col mr-3 items-end'>
                         <small>Hello</small>
                         <p>Oluwaleke Ojo</p>
                     </div>
-                    <img src="https://via.placeholder.com/30" alt="User Profile" />
+                    <img className='rounded-full w-10 h-10' src="https://via.placeholder.com/30" alt="User Profile" />
                 </ListItem>
             </ul>
         </nav>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,7 @@ import { searchBarType } from '../types'
 
 const SearchBar = ({ placeholder }: searchBarType) => {
     return (
-        <form className='bg-gray-50 flex items-center rounded-sm p-1'>
+        <form className='bg-gray-100 flex items-center rounded-lg p-1 w-11/12 max-w-sm'>
             <input
                 className='bg-transparent flex-grow p-1 outline-none' type="text"
                 placeholder={placeholder} />
